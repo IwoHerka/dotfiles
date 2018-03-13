9c6815d52f398de46f37c8aed71db07c4dcda3ed
Iwo Herka <hi@iwoherka.eu>
2018-03-13 15:52:49 +0000

    modifed: init_vim.sh

--- init_vim.sh
+++ init_vim.sh
@@ -6,7 +6,9 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Setup commentary.
-cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-commentary.git
-cd ~
+git clone git://github.com/tpope/vim-commentary.git ~/.vim/bundle/commentary
 
+# Setup airline.
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
+# Remember to run :Helptags
