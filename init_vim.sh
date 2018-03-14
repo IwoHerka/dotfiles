267484a44604efdd681a70ea9b30f82dfebaf140
Iwo Herka <hi@iwoherka.eu>
2018-03-14 09:07:55 +0000

    modified: init_vim.sh

--- init_vim.sh
+++ init_vim.sh
@@ -12,10 +12,12 @@ git clone git://github.com/tpope/vim-commentary.git ~/.vim/bundle/commentary
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 
 # Setup solarized airline.
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+git clone https://github.com/vim-airline/vim-airline-themes \
+    ~/.vim/bundle/vim-airline-themes
 
 # Setup solarized scheme.
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/solarized
+git clone git://github.com/altercation/vim-colors-solarized.git \
+    ~/.vim/bundle/solarized
 
 # Setup nerdtree.
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
