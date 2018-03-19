5f9405d70e619121ad8cda52ea18d661f038f69b
Iwo Herka <iwo.herka@makimo.pl>
2018-03-19 14:27:16 +0100

    modified: get_msg.py

--- get_msg.py
+++ get_msg.py
@@ -2,6 +2,6 @@
 import sys
 lines = []
 for l in sys.stdin.readlines():
-    if 'new file' in l or 'modified' in l or 'deleted' in l:
+    if 'new file:' in l or 'modified:' in l or 'deleted:' in l:
         lines.append(' '.join(l.strip().split()))
 sys.stdout.write('; '.join(lines))
